b5f22008b62c58bfa38ac599047461d13bdc5a25
zjuzqj <michaelchouqj@gmail.com>
2016-07-14 17:25:48 +0800

    leetcode116 3Sum Code

--- lc16.py
+++ lc16.py
@@ -1,26 +1,21 @@
 class Solution(object):
-    def threeSum(self, nums, target):
+    def threeSumClosest(self, nums, target):
         nums.sort()
-        clos
+        diff = 0x7FFFFFFF
         for i in xrange(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
             l, r = i+1, len(nums) - 1
-            while r > l:
+            while l < r:
                 s = nums[i] + nums[l] + nums[r]
-                if s > 0:
-                    r = r - 1
-                elif s < 0:
-                    l = l + 1
+                if s == target:
+                    return target
+                if abs(s - target) < diff:
+                    s1 = s
+                    diff = abs(s - target)
+                if s > target:
+                    r -= 1
                 else:
-                    res = [nums[i], nums[l], nums[r]]
-                    while l < r and nums[l] == nums[l+1]:
-                        l = l + 1
-                    while l < r and nums[r] == nums[r-1]:
-                        r = r - 1
-                    l = l + 1
-                    r = r - 1
-        return res
+                    l += 1
+        return s1
 
 solution = Solution()
-print
+print solution.threeSumClosest([0,2,1,-3], 1)
