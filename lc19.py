9e1aeb9da3a419baa1a119aec4add0e1384e7898
zjuzqj <michaelchouqj@gmail.com>
2016-07-18 11:41:29 +0800

    leetcode problem 19 finished not by oneself

---
+++ lc19.py
@@ -0,0 +1,28 @@
+Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        res = ListNode(0)
+        res.next = head
+        first, second = res, res
+        for i in xrange(n):
+            first = first.next
+        while first.next:
+            # when first comes to the last one
+            # second comes to the n+1 from the end
+            first = first.next
+            second = second.next
+        second.next = second.next.next
+
+        return res.next
+
+
