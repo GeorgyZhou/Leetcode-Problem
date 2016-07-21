5fec34b670c77539220c6dcacbb9b0fee8fd72b0
zjuzqj <michaelchouqj@gmail.com>
2016-07-21 21:19:19 +0800

    leetcode problem 26

---
+++ lc26.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeDuplicates(self, A):
+        if not A:
+            return 0
+
+        newTail = 0
+
+        for i in range(1, len(A)):
+            if A[i] != A[newTail]:
+                newTail += 1
+                A[newTail] = A[i]
+
+        return newTail + 1
+
+solution = Solution()
+print solution.removeDuplicates([1, 1, 2])
