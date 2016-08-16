f0aa7de8c0c1439543b51a37b7ffd259ef18986a
zjuzqj <michaelchouqj@gmail.com>
2016-08-16 02:38:13 -0700

    leetcode problem 44 with problems untackled

---
+++ lc44(2).py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        lens = len(s)
+        if len(p) - p.count('*') > lens:
+            return False
+        dp = [True] + [False] * lens
+        for i in p:
+            if i != '*':
+                for j in range(lens-1, -1, -1):
+                    dp[j+1] = dp[j] and (s[j] == i or i == '?')
+            else:
+                for j in xrange(lens):
+                    dp[j+1] = dp[j] or dp[j+1]
+            dp[0] = dp[0] and i == '*'
+        return dp[lens]
+
+solution = Solution()
+print solution.isMatch('aa','a')
---
+++ lc44.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        lens, lenp = len(s), len(p)
+        dp = [[False] * (lenp + 1) for _ in xrange(lens + 1)]
+        dp[0][0] = True
+        flag = False
+        if s and not p:
+            return False
+        i = 0
+        while i < lenp and p[i] == '*':
+            dp[0][i+1] = True
+            i += 1
+        for i in xrange(lens):
+            for j in xrange(lenp):
+                if p[j] == '*':
+                    dp[i+1][j+1] = dp[i+1][j] or dp[i][j] or dp[i][j+1]
+                elif p[j] == '?' or p[j] == s[i]:
+                    dp[i+1][j+1] = dp[i][j]
+        return dp[lens][lenp]
+
+solution = Solution()
+print solution.isMatch('c', '*?*')
