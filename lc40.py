767fb843b69de98f284b63f5ee482b4486b425b6
zjuzqj <michaelchouqj@gmail.com>
2016-08-24 20:24:44 -0700

    leetcode problem 40 tackled

--- lc40.py
+++ lc40.py
@@ -5,7 +5,28 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        for i in candidates:
-            if i < target:
+        ret = []
+        for i in xrange(len(candidates)):
+            if candidates[i] < target:
+                for res in Solution.combinationSum(self, candidates, target - candidates[i], i + 1):
+                    res.append(candidates[i])
+                    res.sort()
+                    if res not in ret:
+                        ret.append(res)
+            elif candidates[i] == target and [candidates[i]] not in ret:
+                    ret.append([candidates[i]])
+        return ret
 
-                Solution.combinationSum2(self, candidates.remove(i))
+    def combinationSum(self, candidates, target, start):
+        ret = []
+        for i in xrange(start, len(candidates), 1):
+            if candidates[i] < target:
+                for res in Solution.combinationSum(self, candidates, target - candidates[i], i + 1):
+                    res.append(candidates[i])
+                    ret.append(res)
+            elif candidates[i] == target:
+                ret.append([candidates[i]])
+        return ret
+
+solution = Solution()
+print solution.combinationSum2([10, 1, 2, 7, 6, 1, 5], 8)
