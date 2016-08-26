11da95b871f0147c8d7a5d92644e40cd94ae6f5a
zjuzqj <michaelchouqj@gmail.com>
2016-08-25 17:57:37 -0700

    to merge lc40

--- lc40.py
+++ lc40.py
@@ -5,7 +5,20 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        for i in candidates:
-            if i < target:
 
-                Solution.combinationSum2(self, candidates.remove(i))
+        ret = []
+
+        for i in xrange(len(candidates)):
+            if i == target:
+                ret.append([i])
+            elif i < target:
+                for former in Solution.combinationSum2(self, candidates, target - i):
+                    if i not in former:
+                        former.append(i)
+                        former.sort()
+                        if former not in ret:
+                            ret.append(former)
+        return ret
+
+solution = Solution()
+print solution.combinationSum2([1,2,3,5,9],10)
