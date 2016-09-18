c5bd5f4da268264d60c62dab045574b4c858c6f4
zjuzqj <michaelchouqj@gmail.com>
2016-09-18 11:00:57 -0700

    leetcode problem 228

--- lc228.py
+++ lc228.py
@@ -4,4 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[str]
         """
-        
+        if not nums:
+            return []
+        ret = []
+        count = []
+        for i in xrange(len(nums)):
+            if len(count) == 0 or nums[i] == nums[i-1] + 1:
+                count.append(nums[i])
+            else:
+                if len(count) == 1:
+                    ret.append(str(count[0]))
+                else:
+                    ret.append(str(count[0])+'->'+str(count[-1]))
+                count[:] = []
+                count.append(nums[i])
+        if len(count) == 1:
+            ret.append(str(count[0]))
+        else:
+            ret.append(str(count[0]) + '->' + str(count[-1]))
+        return ret
+
+s = Solution()
+print s.summaryRanges([0,0,1,1,1])
