de992e1602d57e2e45e2c93c5f91bc92a293f8ad
zjuzqj <michaelchouqj@gmail.com>
2016-10-14 19:13:08 -0700

    leetcode 56

--- lc56.py
+++ lc56.py
@@ -10,6 +10,30 @@ class Solution(object):
         :type intervals: List[Interval]
         :rtype: List[Interval]
         """
-        for i in xrange(len(intervals)-1):
-            for j in xrange(i+1, len(intervals)):
-                if intervals[i].start >= intervals[j].end or intervals
+        ret = []
+        n = len(intervals)
+        if n <= 1:
+            return intervals
+        import heapq
+        heap = []
+        dic = dict()
+        lst = None
+        for i in xrange(n):
+            heapq.heappush(heap, (intervals[i].start, 0, i))
+            heapq.heappush(heap, (intervals[i].end, 1, i))
+        while len(heap) > 0:
+            time, start, index = heapq.heappop(heap)
+            if start == 0:
+                if len(dic) == 1 and lst is None:
+                    lst = intervals[dic.keys()[0]].start
+                dic[index] = 1
+            else:
+                if len(dic) == 1 and index in dic and lst is None:
+                    ret.append(intervals[index])
+                elif len(dic) == 1 and index in dic and lst is not None:
+                    ret.append(Interval(lst, intervals[index].end))
+                    lst = None
+                del dic[index]
+        return ret
+                    
+                
