73dc8f940ac8c9fd67fd3cee07913f7742a95790
zjuzqj <michaelchouqj@gmail.com>
2016-10-21 14:01:30 -0700

    lc102

---
+++ lc102.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        dic = dict()
+        ret = []
+        queue = [[root, 1]]
+        while len(queue) > 0:
+            root, level = queue[0]
+            del queue[0]
+            if root.left:
+                queue.append([root.left, level + 1])
+            if root.right:
+                queue.append([root.right, level + 1])
+            if not dic.has_key(level):
+                ret.append([root.val])
+                dic[level] = 1
+            else:
+                ret[-1].append(root.val)
+        return ret
