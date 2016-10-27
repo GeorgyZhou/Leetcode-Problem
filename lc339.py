15170d168784d49948eb2508cd66b9e5c76753be
zjuzqj <michaelchouqj@gmail.com>
2016-10-27 13:51:27 -0700

    leetcode 339

---
+++ lc339.py
@@ -0,0 +1,39 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution(object):
+    def depthSum(self, nestedList):
+        """
+        :type nestedList: List[NestedInteger]
+        :rtype: int
+        """
+        return self.rec(nestedList,1)
+    
+    def rec(self, nl, w):
+        ret = 0
+        for i in nl:
+            ret += ((w * i.getInteger()) if i.isInteger() else self.rec(i.getList(), w + 1))
+        return ret
+            
