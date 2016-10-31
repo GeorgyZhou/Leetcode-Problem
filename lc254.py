14b73ac4e9fe3d435eacf65b862c0d9debd9ac55
zjuzqj <michaelchouqj@gmail.com>
2016-10-31 12:26:59 -0700

    leetcode 254

--- lc254.py
+++ lc254.py
@@ -1,26 +1,17 @@
 class Solution(object):
-    def __init__(self):
-        self.dic = dict()
-        self.dic[1] = []
-        self.dic[2] = [[2]]
-        self.dic[0] = []
-    
     def getFactors(self, n):
         """
         :type n: int
         :rtype: List[List[int]]
         """
-        from math import sqrt
-        if self.dic.has_key(n):
-            return self.dic[n]
-        ret = []
-        for i in xrange(2, int(sqrt(n))+1):
+        if n == 1:
+            return []
+        return self.rec(n, 2, [], [])
+    
+    def rec(self, n, i, comb, last):
+        while i * i <= n:
             if n % i == 0:
-                ret.append([i, n/i])
-                for com in self.getFactors(n/i):
-                    ret.append([i] + com)
-        self.dic[n] = ret
-        return ret
-                    
-s = Solution()
-print s.getFactors(4)
+                comb.append(last +[i, n/i])
+                self.rec(n/i, i, comb, last + [i])
+            i += 1
+        return comb
