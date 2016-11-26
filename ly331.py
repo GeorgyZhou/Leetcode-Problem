f8b855ea219aa3c1990c6db823e3d3eefa9544ef
zjuzqj <michaelchouqj@gmail.com>
2016-11-26 15:59:00 -0800

    leetcode 331

---
+++ ly331.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValidSerialization(self, preorder):
+        """
+        :type preorder: str
+        :rtype: bool
+        """
+        self.index = 0
+        self.vals = preorder.split(',')
+        self.n = len(self.vals)
+        self.rec()
+        return self.index == self.n
+        
+    def rec(self):
+        val = self.vals[self.index] if self.index < self.n else '#'
+        self.index += 1
+        if val == "#":
+            return
+        self.rec()
+        self.rec()
