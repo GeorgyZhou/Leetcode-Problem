6a143dbfa22cb453c58c4db7190256c3199911c4
Michael Zhou <michaelchouqj@gmail.com>
2018-11-19 11:41:29 -0800

    rename to correct name

--- lc331_1.py
+++ lc331_1.py
@@ -9,7 +9,7 @@ class Solution(object):
         self.n = len(self.vals)
         self.rec()
         return self.index == self.n
-        
+
     def rec(self):
         val = self.vals[self.index] if self.index < self.n else '#'
         self.index += 1
